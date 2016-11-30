07c8fb5089c9b0d8e0cbb8b3c7926fdeb1bf0ea9
taylor <taylor@REVIVAL>
2016-11-29 20:16:19 -0500

    init

---
+++ jacobs33_Final/Models/BudgetSheet.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jacobs33_Final.Models
+{
+    public class BudgetSheet
+    {
+        #region Enum
+        #endregion
+
+        #region Fields
+        private string _name;
+        private string _description;
+        private List<LineItem> _budgetItems;
+        private DateTime _dateCreated;
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public List<LineItem> BudgetItems
+        {
+            get { return _budgetItems; }
+            set { _budgetItems = value; }
+        }
+        public DateTime DateCreated
+        {
+            get { return _dateCreated; }
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// BudgetSheet constructor
+        /// </summary>
+        public BudgetSheet()
+        {
+            _budgetItems = new List<LineItem>();
+            _dateCreated = DateTime.Now;
+        }
+        #endregion
+
+        #region Methods
+        public void SaveBudgetSheet()
+        {
+
+        }
+
+        public void LoadBudgetSheet()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ jacobs33_Final/Models/LineItem.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jacobs33_Final.Models
+{
+    public class LineItem
+    {
+        #region Enum
+        public enum LineItemType
+        {
+            None, Income, Expense, Other
+        }
+        #endregion
+
+        #region Fields
+        private string _name;
+        private string _description;
+        private LineItemType _itemType;
+        private double _itemAmount;
+        private int _itemGroup;
+        private bool _preTax;
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public LineItemType ItemType
+        {
+            get { return _itemType; }
+            set { _itemType = value; }
+        }
+        public double ItemAmount
+        {
+            get { return _itemAmount; }
+            set { _itemAmount = value; }
+        }
+        public int ItemGroup
+        {
+            get { return _itemGroup; }
+            set { _itemGroup = value; }
+        }
+        public bool PreTax
+        {
+            get { return _preTax; }
+            set { _preTax = value; }
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// LineItem constructor
+        /// </summary>
+        public LineItem()
+        {
+
+        }
+        /// <summary>
+        /// Overload for LineItem constructor
+        /// </summary>
+        /// <param name="name">Name of Line Item</param>
+        /// <param name="type">Type of Line Item</param>
+        /// <param name="amount">Line Item amount</param>
+        public LineItem(string name, LineItemType type, double amount)
+        {
+            _name = name;
+            _itemType = type;
+            _itemAmount = amount;
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
